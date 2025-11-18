6e17b518e4a82dde7eff0335af722fce3e6b69b4
jonah <jonahkf@gmail.com>
2025-11-18 13:41:19 +0900

    updated dummy functions in App

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,8 @@
 import "../data/trismegistus-onyx.json";
 
 function App() {
+  // documentStyle.setProperty("--background-color:", settings.theme.textColor);
   // documentStyle.setProperty("--text-color", settings.theme.textColor);
-  // documentStyle.setProperty("--white", settings.theme.white);
-  // documentStyle.setProperty("--gray", settings.theme.gray);
   // documentStyle.setProperty("--black", settings.theme.black);
   // documentStyle.setProperty("--red", settings.theme.red);
   // documentStyle.setProperty("--green", settings.theme.green);
@@ -12,6 +11,8 @@ function App() {
   // documentStyle.setProperty("--blue", settings.theme.blue);
   // documentStyle.setProperty("--magenta", settings.theme.magenta);
   // documentStyle.setProperty("--cyan", settings.theme.cyan);
+  // documentStyle.setProperty("--white", settings.theme.white);
+  // documentStyle.setProperty("--gray", settings.theme.gray);
 
   return (
     <main>
