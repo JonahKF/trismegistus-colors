8071ae1022c158ef5b1f032fd60d577ccf7c7c32
jonah <jonahkf@gmail.com>
2025-11-18 13:22:10 +0900

    Added CSS reset, dummy style settings in App, changed name of json theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
 // import { useState } from "react";
+import "../data/trismegistus-onyx.json";
 
 function App() {
+  // documentStyle.setProperty("--text-color", settings.theme.textColor);
+  // documentStyle.setProperty("--white", settings.theme.white);
+  // documentStyle.setProperty("--gray", settings.theme.gray);
+  // documentStyle.setProperty("--black", settings.theme.black);
+  // documentStyle.setProperty("--red", settings.theme.red);
+  // documentStyle.setProperty("--green", settings.theme.green);
+  // documentStyle.setProperty("--yellow", settings.theme.yellow);
+  // documentStyle.setProperty("--blue", settings.theme.blue);
+  // documentStyle.setProperty("--magenta", settings.theme.magenta);
+  // documentStyle.setProperty("--cyan", settings.theme.cyan);
+
   return (
     <>
       <div>
