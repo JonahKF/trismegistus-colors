726db0eeea00fe5b836f3ff0e488222f7e94d007
jonah <jonahkf@gmail.com>
2025-11-26 15:05:24 +0900

    added prompt styling in terminal

--- src/components/Terminal.jsx
+++ src/components/Terminal.jsx
@@ -3,61 +3,85 @@ import data from "../../data/terminal-data.json";
 export default function Terminal() {
   return (
     <div className="terminal">
-      <div className="ascii">
-        <span className="green">{"                      ..'"}</span>
-        <span className="green">{"                  ,xNMM."}</span>
-        <span className="green">{"                .OMMMMo"}</span>
-        <span className="green">{'                lMMM"'}</span>
-        <span className="yellow">{"      .;loddo:.  .olloddol;."}</span>
-        <span className="yellow">{"    cKMMMMMMMMMMNWMMMMMMMMMM0:"}</span>
-        <span className="yellow">{"  .KMMMMMMMMMMMMMMMMMMMMMMMWd."}</span>
-        <span className="red">{"  XMMMMMMMMMMMMMMMMMMMMMMMX."}</span>
-        <span className="red">{" ;MMMMMMMMMMMMMMMMMMMMMMMM:"}</span>
-        <span className="red">{" :MMMMMMMMMMMMMMMMMMMMMMMM:"}</span>
-        <span className="magenta">{" .MMMMMMMMMMMMMMMMMMMMMMMMX."}</span>
-        <span className="magenta">{"  kMMMMMMMMMMMMMMMMMMMMMMMMWd."}</span>
-        <span className="magenta">{"  'XMMMMMMMMMMMMMMMMMMMMMMMMMMk"}</span>
-        <span className="blue">{"   'XMMMMMMMMMMMMMMMMMMMMMMMMK."}</span>
-        <span className="blue">{"     kMMMMMMMMMMMMMMMMMMMMMMd"}</span>
-        <span className="cyan">{"      ;KMMMMMMMWXXWMMMMMMMk."}</span>
-        <span className="cyan">{'        "cooc*"    "*coo""'}</span>
-      </div>
+      <div className="terminal-top-wrapper">
+        <div className="ascii">
+          <span className="green">{"                      ..'"}</span>
+          <span className="green">{"                  ,xNMM."}</span>
+          <span className="green">{"                .OMMMMo"}</span>
+          <span className="green">{'                lMMM"'}</span>
+          <span className="yellow">{"      .;loddo:.  .olloddol;."}</span>
+          <span className="yellow">{"    cKMMMMMMMMMMNWMMMMMMMMMM0:"}</span>
+          <span className="yellow">{"  .KMMMMMMMMMMMMMMMMMMMMMMMWd."}</span>
+          <span className="red">{"  XMMMMMMMMMMMMMMMMMMMMMMMX."}</span>
+          <span className="red">{" ;MMMMMMMMMMMMMMMMMMMMMMMM:"}</span>
+          <span className="red">{" :MMMMMMMMMMMMMMMMMMMMMMMM:"}</span>
+          <span className="magenta">{" .MMMMMMMMMMMMMMMMMMMMMMMMX."}</span>
+          <span className="magenta">{"  kMMMMMMMMMMMMMMMMMMMMMMMMWd."}</span>
+          <span className="magenta">{"  'XMMMMMMMMMMMMMMMMMMMMMMMMMMk"}</span>
+          <span className="blue">{"   'XMMMMMMMMMMMMMMMMMMMMMMMMK."}</span>
+          <span className="blue">{"     kMMMMMMMMMMMMMMMMMMMMMMd"}</span>
+          <span className="cyan">{"      ;KMMMMMMMWXXWMMMMMMMk."}</span>
+          <span className="cyan">{'        "cooc*"    "*coo""'}</span>
+        </div>
 
-      <div className="terminal-output">
-        <span>
-          <span className="green">{data.header.username}</span>@
-          <span className="green">{data.header.hostname}</span>
-        </span>
-        <span>-------------</span>
-        {Object.entries(data.systemInfo).map(([key, value]) => (
-          <span key={key}>
-            <span className="yellow">{key}: </span>
-            {value}
+        <div className="terminal-output">
+          <span>
+            <span className="green">{data.header.username}</span>@
+            <span className="green">{data.header.hostname}</span>
           </span>
-        ))}
-        <br />
-        <div className="output-colors">
-          <div className="color-row">
-            <div className="black"></div>
-            <div className="red"></div>
-            <div className="green"></div>
-            <div className="yellow"></div>
-            <div className="blue"></div>
-            <div className="magenta"></div>
-            <div className="cyan"></div>
-            <div className="gray"></div>
-          </div>
-          <div className="color-row">
-            <div className="light-black"></div>
-            <div className="light-red"></div>
-            <div className="light-green"></div>
-            <div className="light-yellow"></div>
-            <div className="light-blue"></div>
-            <div className="light-magenta"></div>
-            <div className="light-cyan"></div>
-            <div className="white"></div>
+          <span>-------------</span>
+          {Object.entries(data.systemInfo).map(([key, value]) => (
+            <span key={key}>
+              <span className="yellow">{key}: </span>
+              {value}
+            </span>
+          ))}
+
+          <br />
+
+          <div className="output-colors">
+            <div className="color-row">
+              <div className="black"></div>
+              <div className="red"></div>
+              <div className="green"></div>
+              <div className="yellow"></div>
+              <div className="blue"></div>
+              <div className="magenta"></div>
+              <div className="cyan"></div>
+              <div className="gray"></div>
+            </div>
+            <div className="color-row">
+              <div className="light-black"></div>
+              <div className="light-red"></div>
+              <div className="light-green"></div>
+              <div className="light-yellow"></div>
+              <div className="light-blue"></div>
+              <div className="light-magenta"></div>
+              <div className="light-cyan"></div>
+              <div className="white"></div>
+            </div>
           </div>
         </div>
+      </div>
+
+      <div className="prompt">
+        <span>
+          <span className="yellow">terminal-colors </span>
+          on
+          <span className="red">
+            {" "}
+            <i className="nf nf-oct-git_branch"></i> main{" "}
+          </span>
+          via
+          <span className="green">
+            {" "}
+            <i className="nf nf-dev-nodejs_small"></i> v22.20.0
+          </span>
+        </span>
+        <span>
+          <span className="green">❯</span>
+          <span className="cursor"> |</span>
+        </span>
       </div>
     </div>
   );
